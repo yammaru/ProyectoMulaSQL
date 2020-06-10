c5ec91fbf99ecb4f2d2bef7fb6e3c6eab0a49a15
yammaru <63098405+yammaru@users.noreply.github.com>
2020-06-10 14:22:36 -0500

    Add files via upload

---
+++ BLL/ConnectionSQL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace BLL
+{
+    class ConnectionSQL
+    {
+        string cadena = @"Data Source=USUARIO\SQLEXPRESS;Initial Catalog=Mula;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection connection = new SqlConnection();
+        public ConnectionSQL()
+        {
+            connection.ConnectionString = cadena;
+        }
+        public SqlConnection ConeXXXion() 
+        { 
+            return connection; 
+        }
+        public void abrir()
+        {
+                connection.Open();
+        }
+        public void Cerrar() 
+        { 
+            connection.Close(); 
+        }
+    }
+}
---
+++ BLL/MovimientoService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using DAL;
+namespace BLL
+{
+    public class MovimientoService
+    {
+        ConnectionSQL connection = new ConnectionSQL();
+        MovimientoRepository RepositorioMovimiento;
+        public MovimientoService()
+        {
+            RepositorioMovimiento = new MovimientoRepository(connection.ConeXXXion());
+        }
+        public string Guardar(Movimiento movimiento)
+        {
+            try
+            {
+                connection.abrir();
+                RepositorioMovimiento.GuardarMovimiento(movimiento);
+                connection.Cerrar();
+                return $"el producto {movimiento.CodigoProducto}, Se modifico.";
+            }
+            catch (Exception e)
+            {
+                connection.Cerrar();
+                return "Error en Bases de datos al modificar::: " + e.Message;
+            }
+            
+
+
+        }
+        public List<Movimiento> MostrarMovimiento()
+        {
+            try
+            {
+                connection.abrir();
+                List<Movimiento> movimientos = RepositorioMovimiento.ConsultarMovimiento();
+
+                connection.Cerrar();
+                return movimientos;
+            }
+            catch (Exception )
+            {
+                connection.Cerrar();
+                return null;
+            }
+        }
+        public List<Movimiento> Buscar(string tipo, string  code , DateTime fecha)
+        {
+
+            connection.abrir();
+            List<Movimiento> movimientos = RepositorioMovimiento.ConsultarMovimiento();
+
+            connection.Cerrar();
+           
+                return movimientos.Where(M=>M.Tipo.Equals(tipo)||M.CodigoProducto.Equals(code)||M.Fecha.Equals(fecha)).ToList();
+            
+        }
+    }
+}
