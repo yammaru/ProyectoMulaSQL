9362c657ad00b7360b1333b00043febd53f52b36
yammaru <63098405+yammaru@users.noreply.github.com>
2020-06-10 14:10:59 -0500

    Add files via upload

---
+++ Movimiento.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Movimiento] (
+    [CodigoProducto] NVARCHAR (15) NOT NULL,
+    [Tipo]           NVARCHAR (20) NULL,
+    [Cantidad]       INT           NULL,
+    [Fecha]          DATETIME      NULL
+);
+
