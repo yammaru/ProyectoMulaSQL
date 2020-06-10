d0d2b222a1e2d104cd2be880a0af3432ab52c957
yammaru <63098405+yammaru@users.noreply.github.com>
2020-06-10 14:20:37 -0500

    Add files via upload

---
+++ Entity/Movimiento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Movimiento
+    {
+       public string CodigoProducto { set; get; }
+        public string Tipo { set; get; }
+        public DateTime Fecha { set; get; }
+        public int Cantidad { set; get; }
+        public override string ToString()
+        {
+            return $"{CodigoProducto};{Tipo};{Fecha};{Cantidad}";
+        }
+       
+    
+    }
+}
---
+++ Entity/Producto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Producto
+    {
+        public string CodigoProducto { set; get; }
+        public string NombreProducto { set; get; }
+        public int Existencia { set; get; }      
+        public string Referencia { set; get; }
+        public string Marca { set; get; }
+
+        List<Movimiento> movimientos = new List<Movimiento>();
+        public Producto()
+        {
+           
+        }
+
+        public Producto(string codigoProducto, string nombreProducto, int cantidad,string referencia,string marca)
+        {
+            CodigoProducto = codigoProducto;
+            NombreProducto = nombreProducto;
+            Existencia = cantidad;
+            Referencia = Referencia;
+            Marca = marca;
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{CodigoProducto};{NombreProducto};{Existencia};{Referencia};{Marca}";
+        }
+        //public void CalculaExistencia(Movimiento movimiento)
+        //{
+        //    Existencia = (movimiento.Tipo == "Entra") ? Existencia += movimiento.Cantidad : Existencia -= movimiento.Cantidad;
+        //}
+
+
+    }
+}
